65279389ee948f0654b0c8996b7dd6702130cb67
Alumno <Alumno@EQ28-LAB1>
2014-04-09 19:38:10 -0300

    ventana

---
+++ lab3/src/lab3/ventana.java
@@ -0,0 +1,14 @@
+package lab3;
+import javax.swing.JFrame;
+
+public class ventana {
+	
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("mi Primera ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}

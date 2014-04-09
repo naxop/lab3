a82cd35d523bff96789c22711bc63ecf19972990
Alumno <Alumno@EQ28-LAB1>
2014-04-09 19:58:22 -0300

    mejor ventana

--- lab3/src/lab3/ventana.java
+++ lab3/src/lab3/ventana.java
@@ -1,11 +1,18 @@
 package lab3;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class ventana {
 	
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("hola soy una ventana");
+		frame.add(label);
 		frame.setTitle("mi Primera ventana");
 		frame.setSize(800, 600);
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
